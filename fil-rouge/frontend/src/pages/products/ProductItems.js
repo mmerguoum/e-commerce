21c0b39456fa3180d370004d02c8cd09f691a019
mmerguoum <merguoummourad@gmail.com>
2022-06-23 13:37:16 +0000

    update product

--- fil-rouge/frontend/src/pages/products/ProductItems.js
+++ fil-rouge/frontend/src/pages/products/ProductItems.js
@@ -25,10 +25,10 @@ const ProductItems = () => {
 
            },[]);
 
-console.log('productPictures : ',product.productPictures && product.productPictures[0].img)
+// console.log('productPictures : ',product.productPictures && product.productPictures[0].img)
   return (
     <Layout>
-    <main className="flex items-center px-36 w-full h-full bg-white">
+    <main className="flex items-center px-36 w-full h-screen bg-white">
 	<div className="border-t border-b pt-12 grid grid-cols-2 gap-8">
 		<div className="flex flex-col justify-start">
 			<div className="flex flex-col w-5/6 object-cover h-5/6 justify-items-start border rounded-lg overflow-hidden"
@@ -38,13 +38,13 @@ console.log('productPictures : ',product.productPictures && product.productPictu
 			</div>
 			<div className="flex flex-col mt-12">
 				<div className="flex flex-col gap-4">
-					<h1 className="capitalize text-3xl font-extrabold">{product.name}</h1>
-					<h2 className="text-2xl">{product.price} درهم</h2>
+				<h1 className="capitalize text-3xl font-extrabold text-gray-700">{product.name}</h1>
+				<h1 className="capitalize text-2xl font-bold text-yellow-400">{product.category}</h1>
 					<p className="text-md text-gray-500	">{product.description}</p>
-					<div className="flex items-center gap-4 my-6 cursor-pointer ">
-						<button className="bg-yellow-600 px-5 py-2 text-white rounded-lg w-1/4 text-center">Add to cart</button>
-	
-					</div>
+						<div className="flex justify-between mt-12">
+							<button className="bg-yellow-600 px-5 py-2 text-white rounded-lg w-1/4 text-center cursor-pointer">Add to cart</button>
+							<h2 className="text-2xl font-bold text-yellow-700">{product.price} درهم</h2>
+						</div>
 				</div>
 			</div>
 		</div>
